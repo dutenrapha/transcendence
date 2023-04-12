cb8de5fe35e27ac8c1af07b9c65ee1496d5bb2f9
Paulo Cunha <pcunha00@hotmail.com>
2023-04-12 17:27:01 -0300

    Add card css styles

--- frontend-v2/src/components/Channels/ChannelCard.tsx
+++ frontend-v2/src/components/Channels/ChannelCard.tsx
@@ -4,11 +4,21 @@ function ChannelCard(props: any) {
   const channel = props.values;
   console.log('channel:', channel);
 
+  const cardStyle = {
+    backgroundColor: '#222222',
+  };
+  const textStyle = {
+    color: 'white',
+    fontWeight: 'bold',
+  };
+
   return (
-    <Card shadow='xl' padding='sm' radius='md' withBorder>
+    <Card style={cardStyle} shadow='xl' padding='sm' radius='md' withBorder>
       <Group position='apart' mt='md' mb='xs'>
-        <Text weight={500}>{channel.name}</Text>
-        <Badge color='pink' variant='light'>
+        <Text style={textStyle} weight={500}>
+          {channel.name}
+        </Text>
+        <Badge color='pink' variant='filled'>
           {channel.channelType}
         </Badge>
       </Group>
