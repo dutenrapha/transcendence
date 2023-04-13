3a97881a8adcba10659038bba5a0efe6654b1bc3
Paulo Cunha <pcunha00@hotmail.com>
2023-04-12 22:35:44 -0300

    Implement modal with PasswordInput component

--- frontend-v2/src/components/Channels/ChannelCard.tsx
+++ frontend-v2/src/components/Channels/ChannelCard.tsx
@@ -1,5 +1,7 @@
-import { Card, Text, Badge, Group } from '@mantine/core';
-import { useState } from 'react';
+import { Card, Text, Badge, Group, Modal, PasswordInput, Button } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useDisclosure } from '@mantine/hooks';
+import { FC, useState } from 'react';
 
 function ChannelCard(props: any) {
   const channel = props.values;
@@ -7,34 +9,74 @@ function ChannelCard(props: any) {
   const [name, setName] = useState<string>(channel.name);
   const [channelType, setChannelType] = useState<string>(channel.channelType);
   const [password, setPassword] = useState<string>(channel.password);
+  const [opened, { open, close }] = useDisclosure(false);
+  const [value, setValue] = useState(null);
 
-  console.log('channel:', channel);
-  console.log('pass: ', password);
+  //  console.log('channel:', channel);
+  //  console.log('pass: ', password);
 
   const cardStyle = {
     backgroundColor: '#222222',
   };
+
   const textStyle = {
     color: 'white',
     fontWeight: 'bold',
   };
 
-  const ProcessClick = () => {
-    if (channelType == 'Password') alert('protegido');
-    else alert('publico');
+  const ProcessCardClick = () => {
+    if (channelType != 'Password') alert('Wellcome to chat !!');
+    else open();
+  };
+
+  const PasswordForm: FC = () => {
+    const handleSubmit = (values: { password: string }) => {
+      console.log('values: ', values);
+    };
+
+    const form = useForm<{ password: string }>({
+      initialValues: {
+        password: '',
+      },
+    });
+
+    return (
+      <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+        <PasswordInput
+          label='Password'
+          placeholder='Your password'
+          required
+          {...form.getInputProps('password')}
+        />
+        <Button type='submit'>Enter</Button>
+      </form>
+    );
   };
 
   return (
-    <Card style={cardStyle} onClick={ProcessClick} shadow='xl' padding='sm' radius='md' withBorder>
-      <Group position='apart' mt='md' mb='xs'>
-        <Text style={textStyle} weight={500}>
-          {name}
-        </Text>
-        <Badge color='pink' variant='filled'>
-          {channelType}
-        </Badge>
-      </Group>
-    </Card>
+    <>
+      <Modal opened={opened} onClose={close} title='Please enter channel password'>
+        <PasswordForm />
+      </Modal>
+      <Card
+        style={cardStyle}
+        onClick={ProcessCardClick}
+        shadow='xl'
+        padding='sm'
+        radius='md'
+        withBorder
+      >
+        <Group position='apart' mt='md' mb='xs'>
+          <Text style={textStyle} weight={500}>
+            {name}
+          </Text>
+          <Badge color='pink' variant='filled'>
+            {channelType}
+          </Badge>
+        </Group>
+      </Card>
+    </>
   );
 }
+
 export default ChannelCard;
